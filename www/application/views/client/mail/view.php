e993570f9c9aab39c1aba2bdfed671beca8be88b
Alies Lapitski <nonyxus@gmail.com>
2014-07-20 20:13:38 +0300

    up

--- www/application/views/client/mail/view.php
+++ www/application/views/client/mail/view.php
@@ -1,13 +1,30 @@
-<?=$mail->subject; ?>
-<?=$mail->description; ?>
-<?=$mail->from; ?>
-<?=$mail->to; ?>
-<?=$mail->created_at; ?>
-
-
-<?= ($mail->kind == 'p') ? 'Parcel' : 'Mail'; ?>
-
-<a href="/client/mail/archive/<?= $mail->id; ?>">Set <?= $mail->archived ? 'not' : ''; ?> archived</a>
-<th><a href="/client/mail/hold/<?= $mail->id; ?>">Set <?= $mail->held ? 'Unhold' : 'Hold'; ?></a></th>
-<th><a href="/client/mail/deliver/<?= $mail->id; ?>">Set <?= $mail->delivered ? 'not' : ''; ?> delivered</a></th>
-<td><a href="/client/mail/delete/<?= $mail->id; ?>">delete</a></td>
+<div class="page-header">
+    <h1><?= ($mail->kind == 'p') ? 'Parcel' : 'Mail'; ?>: <?=$mail->subject; ?></h1>
+</div>
+<p class="lead">
+    <?=$mail->created_at; ?><br>
+    <?=$mail->description; ?>
+</p>
+from: <?=$mail->from; ?><br>
+to: <?=$mail->to; ?><br><br><br>
+<p>
+    <a href="/client/mail/archive/<?= $mail->id; ?>">Set <?= $mail->archived ? 'not' : ''; ?> archived</a> |
+    <a href="/client/mail/hold/<?= $mail->id; ?>">Set <?= $mail->held ? 'Unhold' : 'Hold'; ?></a> |
+    <a href="/client/mail/deliver/<?= $mail->id; ?>">Set <?= $mail->delivered ? 'not' : ''; ?> delivered</a>
+</p>
+<?php if($mail->files->count_all() > 0) { ?>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <h2>Documents</h2>
+<?php } ?>
+<ul>
+    <? foreach($mail->files->find_all() as $file) { ?>
+        <li>
+            Direct link: <a href="/upload/<?=$file->file; ?>" target="_blank"><?=$file->file; ?></a><br><br><br>
+            <iframe src="/upload/<?=$file->file; ?>" frameborder="0" width="100%" height="300"></iframe>
+        </li>
+    <?php } ?>
+</ul>
+<script>$('input[type=file]').bootstrapFileInput();</script>

b166c8dfd24add9a258de928fdeb29cf72b2b20b
serpento <simsonne@tut.by>
2014-07-21 00:04:06 +0300

    up

--- www/application/views/client/mail/index.php
+++ www/application/views/client/mail/index.php
@@ -19,19 +19,18 @@
                             <div class="number">#<?=$mail->id; ?></div>
                             <div class="date"><?=$mail->created_at; ?></div>
                             <div class="kind"><?=($mail->kind == 'p') ? 'Parcel' : 'Mail'; ?></div>
-                            <div><a href="/client/mail/view/<?= $mail->id; ?>"><?=$mail->subject; ?></a></div>
-                        </div>
-                        <div class="row-block">
-                            <div class="from-to-block">From: <?=$mail->from; ?></div>
-                            <div class="from-to-block">To: <?=$mail->to; ?></div>
-                        </div>
-                        <div class="row-block">
+                            <div class="from-to-block">
+                                From: <?=$mail->from; ?> To: <?=$mail->to; ?>
+                            </div>
                             <div class="status-block">Archived: <a href="/client/mail/archive/<?= $mail->id; ?>"><?=$mail->archived; ?></a></div>
                             <div class="status-block">On hold: <a href="/client/mail/hold/<?= $mail->id; ?>"><?=$mail->held; ?></a></div>
                             <div class="status-block">Delivered: <a href="/client/mail/deliver/<?= $mail->id; ?>"><?=$mail->delivered; ?></a></div>
                         </div>
                         <div class="row-block">
-                            <div class="describe-me"><?=$mail->description; ?></div>
+                            <div class="subject-block"><a href="/client/mail/view/<?= $mail->id; ?>"><?=$mail->subject; ?></a></div>
+                        </div>
+                        <div class="row-block">
+                            <div class="describe-me"><a href="/client/mail/view/<?= $mail->id; ?>"><?=$mail->description; ?></a></div>
                         </div>
                     </td>
             <?php } ?>
--- www/application/views/client/mail/view.php
+++ www/application/views/client/mail/view.php
@@ -1,30 +1,30 @@
-<div class="page-header">
+<div class="content-box mail-information">
     <h1><?= ($mail->kind == 'p') ? 'Parcel' : 'Mail'; ?>: <?=$mail->subject; ?></h1>
-</div>
-<p class="lead">
-    <?=$mail->created_at; ?><br>
-    <?=$mail->description; ?>
-</p>
-from: <?=$mail->from; ?><br>
-to: <?=$mail->to; ?><br><br><br>
-<p>
-    <a href="/client/mail/archive/<?= $mail->id; ?>">Set <?= $mail->archived ? 'not' : ''; ?> archived</a> |
-    <a href="/client/mail/hold/<?= $mail->id; ?>">Set <?= $mail->held ? 'Unhold' : 'Hold'; ?></a> |
-    <a href="/client/mail/deliver/<?= $mail->id; ?>">Set <?= $mail->delivered ? 'not' : ''; ?> delivered</a>
-</p>
-<?php if($mail->files->count_all() > 0) { ?>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <h2>Documents</h2>
-<?php } ?>
-<ul>
-    <? foreach($mail->files->find_all() as $file) { ?>
-        <li>
-            Direct link: <a href="/upload/<?=$file->file; ?>" target="_blank"><?=$file->file; ?></a><br><br><br>
-            <iframe src="/upload/<?=$file->file; ?>" frameborder="0" width="100%" height="300"></iframe>
-        </li>
+
+    <p class="lead">
+        <span class="holder-span">
+            <span class="from-to">from: <?=$mail->from; ?>
+               to: <?=$mail->to; ?>
+            </span>
+            <span class="status">
+               <a href="/client/mail/archive/<?= $mail->id; ?>">Set <?= $mail->archived ? 'not' : ''; ?> archived</a> |
+               <a href="/client/mail/hold/<?= $mail->id; ?>">Set <?= $mail->held ? 'Unhold' : 'Hold'; ?></a> |
+               <a href="/client/mail/deliver/<?= $mail->id; ?>">Set <?= $mail->delivered ? 'not' : ''; ?> delivered</a>
+            </span>
+        </span>
+        <?=$mail->description; ?>
+    </p>
+    <span class="date"><?=$mail->created_at; ?></span>
+    <?php if($mail->files->count_all() > 0) { ?>
+        <h2>Documents</h2>
     <?php } ?>
-</ul>
+    <ul class="documents">
+        <? foreach($mail->files->find_all() as $file) { ?>
+            <li>
+                Direct link: <a href="/upload/<?=$file->file; ?>" target="_blank"><?=$file->file; ?></a><br>
+                <iframe src="/upload/<?=$file->file; ?>" frameborder="0" width="100%" height="300"></iframe>
+            </li>
+        <?php } ?>
+    </ul>
+</div>
 <script>$('input[type=file]').bootstrapFileInput();</script>

0d0a82804273782851cea356e6ca57652a0a2ccb
Alies Lapitski <nonyxus@gmail.com>
2014-07-20 15:50:33 +0300

    up

--- www/application/classes/Controller/Client/Mail.php
+++ www/application/classes/Controller/Client/Mail.php
@@ -1,8 +1,14 @@
 <?php
-class Controller_Client_Mail extends Controller_Client {
 
-    public function action_index() {
-        $this->template->content = View::factory('client/mail/index');
+class Controller_Client_Mail extends Controller_Client
+{
+
+    public function action_index()
+    {
+        $this->template->content = View::factory('client/mail/index')
+            ->bind('mails', $mails);
+
+        $mails = ORM::factory('Mail')->find_all();
     }
 }
 
---
+++ www/application/views/client/mail/index.php
@@ -0,0 +1,38 @@
+<div class="page-header">
+    <h1>Mails</h1>
+</div>
+<p class="lead">Manage mails</p>
+<div class="bs-example">
+    <table class="table table-hover">
+        <thead>
+        <tr>
+            <th>#</th>
+            <th>Subject</th>
+            <th>Description</th>
+            <th>From</th>
+            <th>To</th>
+            <th>Kind</th>
+            <th>Archived</th>
+            <th>On hold</th>
+            <th>Delivered</th>
+            <th>What do you want?</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($mails as $mail) { ?>
+            <tr>
+                <td><?= $mail->id; ?></td>
+                <td><?= $mail->subject; ?></td>
+                <td class="mailDescription"><div><?= $mail->description; ?></div></td>
+                <td><?= $mail->from; ?></td>
+                <td><?= $mail->to; ?></td>
+                <td><?= $mail->kind; ?></td>
+                <th><a href="/client/mail/archive/<?= $mail->id; ?>"><?= $mail->archived; ?></a></th>
+                <th><a href="/client/mail/hold/<?= $mail->id; ?>"><?= $mail->held; ?></a></th>
+                <th><a href="/client/mail/deliver/<?= $mail->id; ?>"><?= $mail->delivered; ?></a></th>
+                <td><a href="/client/mail/delete/<?= $mail->id; ?>">delete</a></td>
+            </tr>
+        <?php } ?>
+        </tbody>
+    </table>
+</div>
